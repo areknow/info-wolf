c99c09e907729a0129bc625b855860e088e17d15
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-04-29 10:34:53 -0400

    feat(ui): new navigation icons and state

--- apps/howl/src/app/common/components/global-nav/global-nav.tsx
+++ apps/howl/src/app/common/components/global-nav/global-nav.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../../../assets/howl-logo.svg';
 import styles from './global-nav.module.scss';
 
-export const GlobalNav = () => (
-  <div className={styles.globalNav}>
-    <div className={styles.logo}>
-      <Logo />
+export const GlobalNav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className={styles.globalNav}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div
+        className={styles.actions}
+        onMouseEnter={() => {
+          setMenuOpen(!menuOpen);
+        }}
+        onMouseLeave={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        <nav className={menuOpen ? styles.open : null}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <span>
+          <i className="gg-more"></i>
+        </span>
+        <span>
+          <i className="gg-dark-mode"></i>
+        </span>
+      </div>
     </div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+  );
+};
