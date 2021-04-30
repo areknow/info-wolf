af619e13458e8c3fd965581796099b992fa2439f
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-04-30 16:49:12 -0400

    feat(ui): animated nav menu

--- apps/howl/src/app/common/components/global-nav/global-nav.tsx
+++ apps/howl/src/app/common/components/global-nav/global-nav.tsx
@@ -1,41 +1,47 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../../../assets/howl-logo.svg';
+import { useDarkModeContext } from '../../context';
 import styles from './global-nav.module.scss';
 
 export const GlobalNav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { dark, toggleDarkMode } = useDarkModeContext();
 
   return (
     <div className={styles.globalNav}>
       <div className={styles.logo}>
         <Logo />
       </div>
-      <div
-        className={styles.actions}
-        onMouseEnter={() => {
-          setMenuOpen(!menuOpen);
-        }}
-        onMouseLeave={() => {
-          setMenuOpen(!menuOpen);
-        }}
-      >
-        <nav className={menuOpen ? styles.open : null}>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <span>
-          <i className="gg-more"></i>
-        </span>
-        <span>
-          <i className="gg-dark-mode"></i>
-        </span>
+      <div className={styles.actions}>
+        <div
+          className={styles.trigger}
+          onMouseEnter={() => {
+            setMenuOpen(!menuOpen);
+          }}
+          onMouseLeave={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <nav className={menuOpen ? styles.open : null}>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.icon}>
+            <i className="gg-more"></i>
+          </div>
+        </div>
+        <button className={styles.trigger} onClick={() => toggleDarkMode()}>
+          <div className={styles.icon}>
+            <i className="gg-dark-mode"></i>
+          </div>
+        </button>
       </div>
     </div>
   );
