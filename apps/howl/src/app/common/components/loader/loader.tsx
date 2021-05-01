fdf3dd08b77f06e824a208db92f14ea1d07b584e
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-05-01 01:13:06 -0400

    feat(ui): loader fill color

--- apps/howl/src/app/common/components/loader/loader.tsx
+++ apps/howl/src/app/common/components/loader/loader.tsx
@@ -12,6 +12,7 @@ const StyledLoader = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  fill: var(--text-color);
 `;
 
 export const Loader = () => {
@@ -25,10 +26,7 @@ export const Loader = () => {
         height="40px"
         viewBox="0 0 50 50"
       >
-        <path
-          fill="#000"
-          d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"
-        >
+        <path d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
           <animateTransform
             attributeType="xml"
             attributeName="transform"
