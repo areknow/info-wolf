0a76c27f578da9e9b9bdb217d191ff911c4fc03b
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-05-11 22:24:42 -0400

    feat(ui): added toast notification for overage recovery

--- apps/howl/src/app/pages/home/usage-summary/constants.ts
+++ apps/howl/src/app/pages/home/usage-summary/constants.ts
@@ -1,6 +1,34 @@
+import { Slide, ToastOptions } from 'react-toastify';
+import { Colors } from '../../../common/types';
+
+/** Store the app startup time */
+export const APP_START_DATE = new Date().valueOf();
+/** Date format used in warnings modal */
 export const DATE_FORMAT = 'h:mm:ss';
+/** Used to store historical plot bands once recovered */
 export const HISTORICAL_BANDS = [];
 /** Percentage that must be exceeded or equal to for warning to appear */
-export const VIOLATION_THRESHOLD = 80;
+export const VIOLATION_THRESHOLD = 10;
 /** Duration that must be exceeded for warning to appear */
-export const VIOLATION_DURATION = 120000;
+export const VIOLATION_DURATION = 10000;
+/** Toast message when CPU violation has ended */
+export const TOAST_RECOVERED_MESSAGE = 'CPU threshold violation has ended.';
+/** Options for the toast message. Pass in dynamic colors for styling. */
+export const TOAST_OPTIONS = (colors: Colors): ToastOptions => {
+  return {
+    position: 'bottom-right',
+    autoClose: 5000,
+    closeButton: false,
+    hideProgressBar: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    transition: Slide,
+    style: {
+      background: colors.surface2,
+      color: colors.text,
+      boxShadow: colors.shadow2,
+    },
+  };
+};
--- apps/howl/src/app/pages/home/usage-summary/usage-summary.tsx
+++ apps/howl/src/app/pages/home/usage-summary/usage-summary.tsx
@@ -1,5 +1,7 @@
 import Highcharts from 'highcharts';
 import { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
 import { DARK_THEME, LIGHT_THEME } from '../../../common/colors';
 import {
@@ -10,7 +12,10 @@ import {
 } from '../../../common/components';
 import { useDarkModeContext, useWsContext } from '../../../common/context';
 import {
+  APP_START_DATE,
   HISTORICAL_BANDS,
+  TOAST_OPTIONS,
+  TOAST_RECOVERED_MESSAGE,
   VIOLATION_DURATION,
   VIOLATION_THRESHOLD,
 } from './constants';
@@ -65,7 +70,12 @@ export const UsageSummary = () => {
   const [configOpen, setConfigOpen] = useState(false);
   const [warningsOpen, setWarningsOpen] = useState(false);
   const [recovered, setRecovered] = useState([]);
+  const [recoveredCount, setRecoveredCount] = useState(0);
   const [chartZoomed, setChartZoomed] = useState(false);
+
+  // Calculate if the app has been running longer than the violation duration
+  const runtimeSurpassedViolationDuration =
+    new Date().valueOf() - APP_START_DATE > duration;
 
   const { dark } = useDarkModeContext();
   const colors = dark ? DARK_THEME : LIGHT_THEME;
@@ -84,7 +94,27 @@ export const UsageSummary = () => {
 
   useEffect(() => {
     setRecovered(recordHistoricalOverage(bands, HISTORICAL_BANDS));
-  }, [bands]);
+    /**
+     * In order to display a toast when a CPU overage event has recovered,
+     * check if the amount of recovered plot bands is larger than the counter.
+     * If so, increment the counter and show a toast ONLY if the application
+     * has been running longer than the duration of the threshold violation time.
+     * This ensures that a toast will not pop up immediately even if there are
+     * historical instances of overage and recovery in the current chart window.
+     */
+    if (recovered.length > recoveredCount) {
+      setRecoveredCount(recovered.length);
+      if (runtimeSurpassedViolationDuration) {
+        toast(TOAST_RECOVERED_MESSAGE, TOAST_OPTIONS(colors));
+      }
+    }
+  }, [
+    bands,
+    colors,
+    recovered.length,
+    recoveredCount,
+    runtimeSurpassedViolationDuration,
+  ]);
 
   return (
     <Card
@@ -99,7 +129,11 @@ export const UsageSummary = () => {
           {chartZoomed && (
             <StyledZoomButton
               onClick={() => {
-                Highcharts.charts[0].zoomOut();
+                // Get instance of time series chart
+                const [chart] = Highcharts.charts;
+                // Call highcharts zoom out function
+                chart.zoomOut();
+                // Reset local chart zoomed state
                 setChartZoomed(false);
               }}
             >
@@ -159,6 +193,7 @@ export const UsageSummary = () => {
         threshold={threshold}
         onZoom={() => setChartZoomed(!chartZoomed)}
       />
+      <ToastContainer />
     </Card>
   );
 };
